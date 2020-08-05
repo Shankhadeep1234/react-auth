1b2db530ab13a165fc3de59037bfdf645b1e5c2a
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-08-05 21:30:49 +0530

    making an authentication request

--- server/router.js
+++ server/router.js
@@ -1,5 +1,14 @@
+const passport = require("passport");
+
 const Authentication = require("./controllers/authentication.js");
+const passportService = require("./services/passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function (app) {
+  app.get("/", requireAuth, function (req, res) {
+    res.send("Hi there");
+  });
   app.post("/signup", Authentication.signup);
 };
